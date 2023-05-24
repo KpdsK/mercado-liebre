3e7672bd99ac97b18b0e6435c81d8f73e569d3d0
pablo.scalora <pablo.scalora@gmail.com>
2023-05-24 20:31:31 -0300

    version 1.0.0

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const path = require('path')
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/register', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.get('/404', (req, res)=>{
+    res.send('Error página no encontrada')
+});
+
+const puerto = process.env.PORT || 3001;
+app.listen(puerto, () => console.log(`Servidor escuchando el puerto ${puerto}`));
